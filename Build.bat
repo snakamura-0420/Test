22f134089c64d49d349f033559b7acd636148e06
snakamura-0420 <snakamura.0420@gmail.com>
2018-02-19 17:55:53 +0900

    カレントディレクトリを取得するように変更

--- Build.bat
+++ Build.bat
@@ -2,7 +2,7 @@ rem Unityのパス
 set UNITY_PATH=D:\Unity\Unity2017.2.0p1\Editor
 
 rem プロジェクトパス
-set PROJECT_PATH=D:\Workspace\AndroidSample
+set PROJECT_PATH=%~dp0
 
 rem Buildパス
 set BUILD_PATH=%PROJECT_PATH%\Build\Android
